fb7954547836cc35b14a2c891a133fc89dfa5b79
MasaadAbdo <mssaadabdo@gmail.com>
2023-07-19 00:51:09 +0000

    deone

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# using puppet to make changes
+
+file {'ect/ssh/ssh_cofig;:
+	ensure => present,
+
+content =>"
+
+	#SSH client configuration
+	host
+	identityfile ~/.ssh/school
+	passwordAuthentication no
+}
